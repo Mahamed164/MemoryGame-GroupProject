0533a80f55a334bd61d8e3d30045cf6414e95892
Mahamed Muhidiin Hassan <maha2421@student.miun.se>
2025-09-09 12:14:15 +0200

    Installerade propertychanged.fody till projektet

--- SUP/ViewModels/StartViewModel.cs
+++ SUP/ViewModels/StartViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PropertyChanged;
 
 namespace SUP.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class StartViewModel
     {
     }
