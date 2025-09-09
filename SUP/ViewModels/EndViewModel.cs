62d15409eeb89d76d966ebc4fde6414568d972f0
Christian Gabrielsson <chga2401@student.miun.se>
2025-09-09 15:44:05 +0200

    Start av view/viewmodel

--- SUP/ViewModels/EndViewModel.cs
+++ SUP/ViewModels/EndViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using PropertyChanged;
 
-namespace SUP.ViewModels
+namespace SUP.ViewModels;
+[AddINotifyPropertyChangedInterface]
+
+public class EndViewModel
 {
-    public class EndViewModel
+    public int Missed { get; set; }
+    public int Moves { get; set; }
+
+    public ICommand SaveScoreCmd { get; }
+    public ICommand RestartCmd { get; }
+
+    public EndViewModel(ICommand saveScoreCmd, ICommand restartCmd)
     {
+        SaveScoreCmd = saveScoreCmd;
+        RestartCmd = restartCmd;
     }
 }
