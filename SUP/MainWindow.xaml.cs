d7a3f4332e23f6126bede7115ef397b89685f675
Erik Åström <erik.anders.astrom@gmail.com>
2025-09-09 13:46:03 +0200

    Kopplat GameView med en mainShellViewModel (ny)
    Kopplat GameView till main window

--- SUP/MainWindow.xaml.cs
+++ SUP/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using SUP.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,9 @@ namespace SUP
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainShellViewModel();
         }
+
+
     }
 }
---
+++ SUP/ViewModels/MainShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SUP.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainShellViewModel
+    {
+        public object CurrentView { get; set; } = new GameViewModel();
+        public MainShellViewModel()
+        {
+
+        }
+
+
+    }
+}
