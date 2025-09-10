71bc588be0a8dd0ff01c691efd1ddb674767cd40
Norna Sannemo <nosa2400@student.miun.se>
2025-09-10 09:53:47 +0200

    lagt in klass för cards

---
+++ SUP/ViewModels/Cards.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUP.ViewModels
+{
+    public class Cards
+    {
+        public int Id { get; set; }
+
+        public int RandomNumber { get; set; }
+    }
+}
