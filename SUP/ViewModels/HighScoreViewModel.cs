ccd9ee9cc1d7722e61b519ff8c2fe2f06e8ef064
Christian Gabrielsson <chga2401@student.miun.se>
2025-09-10 12:04:28 +0200

    La till HighScoreV/VM + ScoreM

--- SUP/ViewModels/HighScoreViewModel.cs
+++ SUP/ViewModels/HighScoreViewModel.cs
@@ -4,13 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SUP.Enums
+namespace SUP.ViewModels
 {
-    public enum FeedbackState
+    public class HighScoreViewModel
     {
-        Wrong,
-        Correct,
-        Finished,
-        Neutral
+        //databaslogik
     }
 }
