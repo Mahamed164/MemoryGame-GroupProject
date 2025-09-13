f01b799ea7dc545742acdfe3a45a8850b8f03734
Christian Gabrielsson <chga2401@student.miun.se>
2025-09-13 16:01:27 +0200

    Gjort vyerna mer förenliga. Mest ändringar till startV/VM

--- SUP/ViewModels/StartViewModel.cs
+++ SUP/ViewModels/StartViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using PropertyChanged;
+using SUP.Commands;
 
 namespace SUP.ViewModels
 {
@@ -13,8 +15,6 @@ namespace SUP.ViewModels
     public class StartViewModel
     {
         private string playerName;
-
-        
 
         public string PlayerName
         {
@@ -25,24 +25,29 @@ namespace SUP.ViewModels
 
                 if (string.IsNullOrEmpty(playerName))
                 {
-                    Greeting = "Player Name";
+                    Greeting = "Spelarnamn:";
                 }
                 else
                 {
-                    Greeting = "Hej " + playerName + ", klicka på Start Game när du är redo!";
+                    Greeting = "Hej " + playerName + ", klicka på spela när du är redo!";
                 }
             }
         }
         public string Greeting { get; set; }
-        public ICommand StartGameCmd { get;}
-        
-        public StartViewModel(ICommand startGameCmd) 
+        public ICommand StartGameCmd { get; }
+
+        public StartViewModel(ICommand startGameCmd)
         {
-            StartGameCmd = startGameCmd;
-            Greeting = "Player Name";
+            StartGameCmd = new RelayCommand(p =>
+            {
+                if (string.IsNullOrWhiteSpace(PlayerName))
+                {
+                    MessageBox.Show("Vänligen ange ditt spelarnamn.", "Spelarnamn");
+                    return;
+                }
+                startGameCmd.Execute(p);
+            });
+            Greeting = "Spelarnamn:";
         }
-
-        
-
     }
 }
