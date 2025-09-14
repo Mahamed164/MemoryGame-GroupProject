55791b61752034ec83c38534bdbb182c5d62f64d
Ellus01 <elina0712@hotmail.com>
2025-09-14 19:48:37 +0200

    fixade så att det står minut/minuter och sekunder efter tiden i endView

--- SUP/ViewModels/EndViewModel.cs
+++ SUP/ViewModels/EndViewModel.cs
@@ -21,6 +21,8 @@ public class EndViewModel
     public ICommand RestartCmd { get; }
     public ICommand HighScoreCmd { get; }
 
+    public string TimeAsText { get; set; } 
+
     public EndViewModel()
     {
 
@@ -33,10 +35,62 @@ public class EndViewModel
         Missed = misses;
         Moves = moves;
         TimerText = timer;
+        TimeAsText = SetTimerText();
+        
     }
 
     public void SetScore()
     {
     }
+
+
+    public string SetTimerText()
+    {
+        string format = @"mm\:ss";
+
+        if(TimeSpan.TryParseExact(TimerText, format, null, out var span))
+        {
+            if(span.TotalSeconds < 60) //Hämtar värdet av TimeSpan och retunerar totala sekunderna 
+            {
+                return (int)span.TotalSeconds + " sekunder"; //ifall det är sekunder skriv tiden +  sekunder
+            }
+            else
+            {
+                int mins = (int)span.TotalMinutes;
+                int secs = (int)span.Seconds; //inte total för då stod det 62 istället för 2 
+                string resultMins = "";
+                string resultSecs = "";
+                if (mins == 1)
+                {
+                    resultMins = " minut och "; /*+ secs + " sekunder";*/ //singular ifall det bara är en minut 
+                    resultSecs = " sekunder";
+
+                }else
+                {
+                    resultMins = " minuter och "; /*+ secs + " sekunder";*/ //plural ifall det är flera minuter 
+                    resultSecs = " sekunder";
+                }
+
+                return mins + resultMins + secs + resultSecs;
+            }
+
+        }
+
+        return TimerText;
+
+        //Källa: https://learn.microsoft.com/en-us/dotnet/api/system.timespan.tryparse?view=net-9.0 - denna började jag med men insåg att det var hh:mm inte mm:ss som behvös i vårt spel
+        //Converts the specified string representation of a time interval to its TimeSpan equivalent and returns a value that indicates whether the conversion succeeded.
+
+        /*
+         * Källa för tryparseexact: https://learn.microsoft.com/en-us/dotnet/api/system.timespan.tryparseexact?view=net-9.0 
+         * Converts the string representation of a time interval to its TimeSpan equivalent, and returns a value that indicates whether the conversion succeeded. 
+         * The format of the string representation must match a specified format exactly. --> måste göra om det till mm:ss (minuter:sekunder) format
+         */
+
+        //Allmän källa om TimeSpan: https://learn.microsoft.com/en-us/dotnet/api/system.timespan?view=net-9.0
+
+    }
+
+
 }
 
