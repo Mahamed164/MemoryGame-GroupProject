a7c0a048181ac24c2c06419bdf06b5e132d2e962
Erik Åström <erik.anders.astrom@gmail.com>
2025-09-26 15:50:28 +0200

    sync

--- SUP/ViewModels/EndViewModel.cs
+++ SUP/ViewModels/EndViewModel.cs
@@ -27,8 +27,7 @@ public class EndViewModel
     public int Missed { get; set; }
     public int Moves { get; set; }
     public bool IsMultiplayer { get; set; }
-    public string TimerText { get; set; }
-    public string TimeAsText { get; set; }
+
     public string TotalTimeInSeconds { get; set; }
     public string EndViewMessage { get; set; }
     public DateTime StartTime { get; set; }
@@ -54,7 +53,7 @@ public class EndViewModel
         RestartCmd = restartCmd;
         HighScoreCmd = highScoreCmd;
         BackToStartCmd = backToStartCmd;
-        TimeAsText = SetTimerText();
+        //TimeAsText = currentResult.TimerText;
         CreateEndViewMessage(winningPlayer);
         ConfettiTimer();
         PlayVictorySound(_audio);
@@ -82,52 +81,6 @@ public class EndViewModel
         {
             EndViewMessage = $"Du hittade alla kort med {CurrentResult.Misses} missar på {CurrentResult.Guesses} drag!\nDet tog {CurrentResult.TimerText}.\n\nBra jobbat!";
         }
-    }
-
-    public string SetTimerText()
-    {
-        string format = @"mm\:ss";
-
-        if (TimeSpan.TryParseExact(TimerText, format, null, out var span))
-        {
-            TotalTimeInSeconds = span.TotalSeconds.ToString();
-
-            if (span.TotalSeconds < 60) //Hämtar värdet av TimeSpan och retunerar totala sekunderna 
-            {
-                return (int)span.TotalSeconds + " sekunder"; //ifall det är sekunder skriv tiden +  sekunder
-            }
-            else
-            {
-                int mins = (int)span.TotalMinutes;
-                int secs = (int)span.Seconds; //inte total för då stod det 62 istället för 2 
-                string resultMins = "";
-                string resultSecs = "";
-                if (mins == 1)
-                {
-                    resultMins = " minut och "; /*+ secs + " sekunder";*/ //singular ifall det bara är en minut 
-                    resultSecs = " sekunder";
-
-                }
-                else
-                {
-                    resultMins = " minuter och "; /*+ secs + " sekunder";*/ //plural ifall det är flera minuter 
-                    resultSecs = " sekunder";
-                }
-                return mins + resultMins + secs + resultSecs;
-            }
-        }
-        return TimerText;
-
-        //Källa: https://learn.microsoft.com/en-us/dotnet/api/system.timespan.tryparse?view=net-9.0 - denna började jag med men insåg att det var hh:mm inte mm:ss som behvös i vårt spel
-        //Converts the specified string representation of a time interval to its TimeSpan equivalent and returns a value that indicates whether the conversion succeeded.
-
-        /*
-         * Källa för tryparseexact: https://learn.microsoft.com/en-us/dotnet/api/system.timespan.tryparseexact?view=net-9.0 
-         * Converts the string representation of a time interval to its TimeSpan equivalent, and returns a value that indicates whether the conversion succeeded. 
-         * The format of the string representation must match a specified format exactly. --> måste göra om det till mm:ss (minuter:sekunder) format
-         */
-
-        //Allmän källa om TimeSpan: https://learn.microsoft.com/en-us/dotnet/api/system.timespan?view=net-9.0
     }
     public async Task ConfettiTimer()
     {
